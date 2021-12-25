7114344d88e65cb806f8111df65434fd9d2f3af9
Rem Sadykhov <94738900+rsadykhov@users.noreply.github.com>
2021-12-25 16:01:47 +0000

    Update Golden_Cross_Death_Cross.py

--- Algotrading/Golden_Cross_Death_Cross.py
+++ Algotrading/Golden_Cross_Death_Cross.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-price_df = pd.read_csv(r"AAPL_data.csv", index_col="Date")
+price_df = pd.read_csv(r"Trading_Strategies\Algotrading\AAPL_data.csv", index_col="Date")
 price_df.index.name = None
 
 def plot_golden_cross_death_cross(df):
