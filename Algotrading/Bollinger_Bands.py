4023467868df9291e510e4dc6c946845f7efa407
Rem Sadykhov <94738900+rsadykhov@users.noreply.github.com>
2021-12-25 16:01:19 +0000

    Update Bollinger_Bands.py

--- Algotrading/Bollinger_Bands.py
+++ Algotrading/Bollinger_Bands.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-price_df = pd.read_csv(r"AAPL_data.csv", index_col="Date")
+price_df = pd.read_csv(r"Trading_Strategies\Algotrading\AAPL_data.csv", index_col="Date")
 price_df.index.name = None
 
 def plot_bollinger_bands(df, sma, n_deviations):
